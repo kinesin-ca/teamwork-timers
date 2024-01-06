84cc742c5bf58bb45f7e0a20880e0369ee3360f4
Ian Roddis <31021769+iroddis@users.noreply.github.com>
2024-01-06 11:40:58 -0400

    updating modules

--- src/ca.kinesin.teamwork-timers.sdPlugin/app.js
+++ src/ca.kinesin.teamwork-timers.sdPlugin/app.js
@@ -1,17 +1,34 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
 
-const myAction = new Action('ca.kinesin.teamwork-timers.action');
+const myAction = new Action('ca.kinesin.teamwork-timers.toggle');
 
 /**
  * The first event fired when Stream Deck starts
  */
 $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
 	console.log('Stream Deck connected!');
+	// console.log('Getting Settings');
+	// $SD.getSettings(context);
+	// console.log('Done Getting Settings');
+});
+
+$SD.onDidReceiveGlobalSettings(({ context, payload }) => {
+	console.log('Got GLOBAL settings for coords ', payload.coordinates);
+});
+
+myAction.onDidReceiveSettings(({ action, event, context, device, payload }) => {
+	console.log('Got settings for coords ', payload.coordinates);
 });
 
 myAction.onKeyUp(({ action, context, device, event, payload }) => {
 	console.log('Your key code goes here!');
+	/*
+	 * payload has:
+	 *		settings -- values to persist
+	 *		coordinates -- coordinates of button
+	 *		state -- zero-based index of current state
+	*/
 });
 
 myAction.onDialRotate(({ action, context, device, event, payload }) => {
